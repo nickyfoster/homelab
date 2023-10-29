25c59750adbb2e4bcde22818e93412c535978291
Nikita Guliaev <nik9516@gmail.com>
2023-10-29 13:02:08 -0300

    Bastion updated

--- hetzner-infra/terraform/bastion.tf
+++ hetzner-infra/terraform/bastion.tf
@@ -3,17 +3,17 @@ resource "hcloud_server" "bastion" {
   image       = "ubuntu-20.04"
   server_type = "cx11"
   location    = "fsn1"
-  ssh_keys    = [16515517]
+  ssh_keys    = [module.kube-hetzner.ssh_key_id]
 }
 
 resource "hcloud_server_network" "server_network_bastion" {
-  network_id = 3499331
+  network_id = module.kube-hetzner.network_id
   server_id  = hcloud_server.bastion.id
   ip         = "10.0.0.2"
 }
 
 resource "hcloud_network_route" "bastion" {
-  network_id  = 3499331
+  network_id  = module.kube-hetzner.network_id
   destination = "10.66.66.0/24"
   gateway     = "10.0.0.2"
 }
